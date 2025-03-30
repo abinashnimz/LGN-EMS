a6b33330c7775ee9f631e8cc77575cea1be924ee
abinashnimz <abinash443steam@gmail.com>
2025-03-30 15:13:20 +0530

    Added Early Project for server, client

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+const App = ()=>{
+  return(
+    <div className='bg-teal-300'>Helow World!</div>
+  )
+}
+
+export default App
---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
